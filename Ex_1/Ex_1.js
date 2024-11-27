c26512e387c0f0330c9f621d87d852edf80be7d0
firas najjar <162709181+firas988@users.noreply.github.com>
2024-11-27 22:02:27 +0200

    Add files via upload

---
+++ Ex_1/Ex_1.js
@@ -0,0 +1,7 @@
+let num = 123;
+
+let num_check_2 = num % 2 == 0;
+let num_check_3 = num % 3 == 0;
+let num_check_5 = num % 5 == 0;
+
+console.log(Number(num_check_2) + Number(num_check_3) + Number(num_check_5));
